0484cf9c491a0f0013f2bf7784c6dbf633d5dc6f
Dewsara <dewsara6@gmail.com>
2020-10-09 02:38:31 +0530

    Some Components Added

---
+++ src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,21 @@
+import React , {Component} from "react";
+import {Card} from "react-bootstrap";
+
+
+const navigatToPendiRequistions = () => {
+
+} ;
+
+
+export default class Dashboard extends Component{
+
+    render() {
+        return (
+            <div className="FirstCard" >
+                <Card>
+                    <Card.Body>This is some text within a card body.</Card.Body>
+                </Card>
+            </div>
+        );
+    }
+}
---
+++ src/Components/Pending Requisitions/PendingRequisitions.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import { Table } from "react-bootstrap";
+import {Button} from "react-bootstrap";
+
+export default class PendingRequisitions extends Component{
+
+    constructor(props) {
+        super();
+    }
+
+
+    render() {
+        return super.render();
+    }
+}
