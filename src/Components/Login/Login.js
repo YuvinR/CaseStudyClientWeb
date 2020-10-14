5a12a18dab152d21eb22fedc8696e4356a36d3d9
Dewsara <dewsara6@gmail.com>
2020-10-14 21:17:44 +0530

    Login UI Added

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import { Button,Card} from 'react-bootstrap';
+import {Button} from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
 
 
-function Login(props){
+
+export default function Login(props){
+
+    return(
+        <div style={{marginTop:'10px' , marginBottom : '20px' , display : 'flex' , justifyContent : 'center'}}>
+            <form  noValidate autoComplete="off">
+                <TextField id="outlined-search" label="Username" type="username" variant="outlined" />
+                <br/><br/>
+                <TextField id="outlined-search" label="Password" type="password" variant="outlined" />
+                <br/>
+                <Button style={{marginTop : '10px' , marginLeft : '10px', borderRadius : '50px', width : '180px'}} color="primary" variant="contained">Login</Button>
+                <br/>
+                <h7>Forgot Password?</h7>
+
+            </form>
+        </div>
+    );
 
 }
 
-export default Login;
+
